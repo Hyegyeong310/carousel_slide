816c8866f93aedf7405132f8d7ad04b5f5d01f55
Hyegyeong310 <hyegyeong310@gmail.com>
2019-09-23 00:34:50 +0900

    Updates

---
+++ precache-manifest.448c8d478b729902eb1c3a97eae57150.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfa1e90ce76f17ea53ec4e86d1e898dc",
+    "url": "/github.io/carousel_slide/index.html"
+  },
+  {
+    "revision": "214c4963d41c46ac969c",
+    "url": "/github.io/carousel_slide/static/js/2.725a9dff.chunk.js"
+  },
+  {
+    "revision": "b131338abcaf1b22e8de",
+    "url": "/github.io/carousel_slide/static/js/main.d5364369.chunk.js"
+  },
+  {
+    "revision": "8c15347ec3ed9b48cfb6",
+    "url": "/github.io/carousel_slide/static/js/runtime-main.bcaffeb8.js"
+  },
+  {
+    "revision": "796a5ac522c5e85c050c13fc1ed7474c",
+    "url": "/github.io/carousel_slide/static/media/left-arrow.796a5ac5.svg"
+  },
+  {
+    "revision": "ff834ff5136eb135676256c3de9e65f9",
+    "url": "/github.io/carousel_slide/static/media/right-arrow.ff834ff5.svg"
+  }
+]);
