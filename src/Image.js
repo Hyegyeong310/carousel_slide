7f53348d3c7a216131fe5c003bb2ecce7679542b
한혜경 <hyegyeong310@gmail.com>
2019-10-09 11:20:27 +0900

    [Fix] 첫번째 요소, 마지막 요소일 때 transition값 `0s`로 수정

--- src/Image.js
+++ src/Image.js
@@ -7,15 +7,18 @@ const Container = styled.div`
   height: 100%;
   position: relative;
   opacity: 0.5;
-  transform: scale(0.7);
-  transition: opacity 0.3s linear,
-    transform 0.3s cubic-bezier(0.455, 0.03, 0.515, 0.955);
+  transform: scale(0.9);
+  transition: ${({ index, imagesLength }) =>
+    index === 0 || index === imagesLength - 1
+      ? `0s`
+      : `opacity 0.3s linear,
+    transform 0.3s cubic-bezier(0.455, 0.03, 0.515, 0.955)`};
   ${({ active }) =>
     active &&
     `
         opacity: 1;
-        transform: scale(1);
-      `}
+     transform: scale(1); 
+    `}
 `;
 const Id = styled.span`
   position: absolute;
@@ -25,38 +28,42 @@ const Id = styled.span`
   font-weight: bold;
   z-index: -1;
   opacity: 0.3;
-  @media all and (min-width: 481px) and (max-width: 767px){
+  @media all and (min-width: 481px) and (max-width: 767px) {
     top: -65px;
     left: -40px;
   }
-  @media all and (min-width:320px) and (max-width: 480px) {
+  @media all and (min-width: 320px) and (max-width: 480px) {
     font-size: 70px;
     top: -60px;
   }
 `;
-const Img = styled.img`
+const Item = styled.img`
   width: 500px;
   height: auto;
-  box-shadow: 0 15px 25px rgba(50, 50, 93, 0.2),
-    0 5px 15px rgba(0, 0, 0, 0.2);
+  box-shadow: 0 15px 25px rgba(50, 50, 93, 0.2), 0 5px 15px rgba(0, 0, 0, 0.2);
   @media all and (min-width: 768px) and (max-width: 1024px) {
     width: 400px;
   }
   @media all and (min-width: 481px) and (max-width: 767px) {
     width: 300px;
   }
-  @media all and (min-width:320px) and (max-width: 480px) {
+  @media all and (min-width: 320px) and (max-width: 480px) {
     width: 230px;
   }
 `;
 
 const Image = ({ values, active }) => {
-  const { id, imageUrl, title } = values;
+  const { id, imageUrl, title, imagesLength } = values;
   return (
     <>
-      <Container className={`img-${id}`} active={active}>
+      <Container
+        className={`img-${id}`}
+        active={active}
+        id={id}
+        imagesLength={imagesLength}
+      >
         <Id>{id < 10 ? `0${id}` : id}</Id>
-        <Img src={imageUrl} alt={title} />
+        <Item src={imageUrl} alt={title} />
       </Container>
     </>
   );
